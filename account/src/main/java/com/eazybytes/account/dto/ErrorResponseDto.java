0fb660afbb5afa3a8ee033ea853270b1bd7193b5
Sundarleo11 <sundarrock1999@gmail.com>
2025-07-04 12:42:33 +0530

    updated schema document in dto

--- account/src/main/java/com/eazybytes/account/dto/ErrorResponseDto.java
+++ account/src/main/java/com/eazybytes/account/dto/ErrorResponseDto.java
@@ -1,21 +1,38 @@
 package com.eazybytes.account.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
+@Schema(
+        name = "ErrorResponse",
+        description = "Schema to hold ErrorResponse information"
+)
 @Data
 @AllArgsConstructor
 public class ErrorResponseDto {
 
+    @Schema(
+            description = "apiPath in the response"
+    )
     private String apiPath;
 
+    @Schema(
+            description = "ErrorCode in the response"
+    )
     private HttpStatus errorCode;
 
+    @Schema(
+            description = "errorMessage in the response"
+    )
     private String errorMessage;
 
+    @Schema(
+            description = "errorTime in the response"
+    )
     private LocalDateTime errorTime;
 
 }
