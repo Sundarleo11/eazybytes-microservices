f3b5ba8c058c53a7e7471c62bc36bb296861a277
Sundarleo11 <sundarrock1999@gmail.com>
2025-07-04 15:21:26 +0530

    entity setup

--- loan/src/main/java/com/easybytes/loan/LoanApplication.java
+++ loan/src/main/java/com/easybytes/loan/LoanApplication.java
@@ -2,8 +2,10 @@ package com.easybytes.loan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class LoanApplication {
 
 	public static void main(String[] args) {
