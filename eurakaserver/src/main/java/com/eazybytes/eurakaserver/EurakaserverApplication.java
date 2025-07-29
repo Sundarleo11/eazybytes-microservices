2c6fb9b15ebaade9fce10101c7a8f5b053b31fea
Sundarleo11 <sundarrock1999@gmail.com>
2025-07-29 17:03:09 +0530

    yml changes

--- eurakaserver/src/main/java/com/eazybytes/eurakaserver/EurakaserverApplication.java
+++ eurakaserver/src/main/java/com/eazybytes/eurakaserver/EurakaserverApplication.java
@@ -2,8 +2,10 @@ package com.eazybytes.eurakaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurakaserverApplication {
 
 	public static void main(String[] args) {
