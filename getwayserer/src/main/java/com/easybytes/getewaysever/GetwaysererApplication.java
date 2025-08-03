b8a02ac0aa2b01d7a067a59045c90607e8a31147
Sundarleo11 <sundarrock1999@gamil.com>
2025-08-04 00:51:51 +0530

    added gatewayserver

---
+++ getwayserer/src/main/java/com/easybytes/getewaysever/GetwaysererApplication.java
@@ -0,0 +1,13 @@
+package com.easybytes.getewaysever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetwaysererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetwaysererApplication.class, args);
+	}
+
+}
---
+++ getwayserer/src/test/java/com/easybytes/getewaysever/GetwaysererApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easybytes.getewaysever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetwaysererApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
