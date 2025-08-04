dd92720cb2b0ebeb5873a3a3d511dd549d94f283
Sundar <91257478+Sundarleo11@users.noreply.github.com>
2025-08-04 21:38:23 +0530

    Feature/getway cross cutting routting (#8)
    * added gatewayserver
    
    * RouteLocator
    
    * updated correlationId
    
    * added health check
    
    * docker compose
    
    ---------
    
    Co-authored-by: Sundarleo11 <sundarrock1999@gamil.com>

---
+++ gatewaysever/src/test/java/com/easybytes/gatewaysever/GetwaysererApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easybytes.gatewaysever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetwaysererApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
