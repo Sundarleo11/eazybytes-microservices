560501df596545a36e16b5fd03b23fb4daabdf70
Sundarleo11 <sundarrock1999@gamil.com>
2025-08-04 09:52:44 +0530

    RouteLocator

--- getwayserer/src/main/java/com/easybytes/getewaysever/GetwaysererApplication.java
+++ getwayserer/src/main/java/com/easybytes/getewaysever/GetwaysererApplication.java
@@ -2,12 +2,35 @@ package com.easybytes.getewaysever;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GetwaysererApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GetwaysererApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GetwaysererApplication.class, args);
+    }
+
+
+    @Bean
+    public RouteLocator eazyBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/eazybank/accounts/**")
+                        .filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://ACCOUNTS"))
+                .route(p -> p
+                        .path("/eazybank/loans/**")
+                        .filters(f -> f.rewritePath("/eazybank/loans/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://LOANS"))
+                .route(p -> p
+                        .path("/eazybank/cards/**")
+                        .filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)", "/${segment}"))
+						.uri("lb://CARDS")).build();
+
+
+    }
 
 }
